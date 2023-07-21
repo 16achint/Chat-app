0bda17444178610e4c9082532fa9c423d4c5eec3
achint <achintj96244@gmail.com>
2023-07-21 15:09:18 +0530

    displayed messages inside chat page

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,20 +1,33 @@
-import React from 'react';
 import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/Profile.context';
 import EditableInput from '../EditableInput';
 import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
 import AvatarUplaodBtn from './AvatarUplaodBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
+    // const userNicknameRef = database
+    //   .ref(`/profiles/${profile.uid}`)
+    //   .child('name');
+
     try {
-      await userNicknameRef.set(newData);
+      // await userNicknameRef.set(newData);
+
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      console.log('update', updates);
+
+      await database.ref().update(updates); // Error
+
       Alert.success('Saved Changes', 2000);
     } catch (error) {
       Alert.error(error.message, 2000);
--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -12,3 +12,32 @@ export function transformToArrayWithId(snapVal) {
       })
     : [];
 }
+
+export async function getUserUpdates(userId, keyToUpdate, value, database) {
+  const updates = {};
+  updates[`/profiles/${userId}/${keyToUpdate}`] = value;
+
+  const getMsgs = database
+    .ref('/messages')
+    .orderByChild('author/uid')
+    .equalTo(userId)
+    .once('value');
+
+  const getRooms = database
+    .ref('/rooms')
+    .orderByChild('lastMessage/author/uid')
+    .equalTo(userId)
+    .once('value');
+
+  const [mSnap, rSnap] = await Promise.all([getMsgs, getRooms]);
+
+  mSnap.forEach(msgSnap => {
+    updates[`/messages/${msgSnap.key}/author/${keyToUpdate}`] = value;
+  });
+
+  rSnap.forEach(roomSnap => {
+    updates[`/room/${roomSnap.key}/lastMessage/author/${keyToUpdate}`] = value;
+  });
+
+  return updates;
+}
