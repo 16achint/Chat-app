dbb939b4bb251b57769621d5594bb9d7c540c4b4
achint <achintj96244@gmail.com>
2023-08-10 00:37:08 +0530

    added service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,17 +14,3 @@ firebase.initializeApp({
 });
 
 firebase.messaging();
-
-// import { initializeApp } from "firebase/app";
-// import { getMessaging } from "firebase/messaging/sw";
-
-// const firebaseApp = initializeApp({
-//     apiKey: 'AIzaSyAZTsnGpJU841ZCMaR20sX0v-rvMFI33bU',
-//     authDomain: 'chat-web-app-623d5.firebaseapp.com',
-//     projectId: 'chat-web-app-623d5',
-//     storageBucket: 'chat-web-app-623d5.appspot.com',
-//     messagingSenderId: '689850280386',
-//     appId: '1:689850280386:web:b7ad2483efeb8ef26e8e5b',
-//   });
-
-// const messaging = getMessaging(firebaseApp);
