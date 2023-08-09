951d165b8a2fd474fc46060823a08e968d941064
achint <achintj96244@gmail.com>
2023-08-10 00:25:43 +0530

    added firebase messaging and store

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -15,10 +15,6 @@ const config = {
   messagingSenderId: '689850280386',
   appId: '1:689850280386:web:b7ad2483efeb8ef26e8e5b',
 };
-
-export const fcmVapidKey =
-  'BGwqAb_5soMWQmnpLrye4Guv74Wqj5jk9FqGBl2GZLl4FuU8EJgKEr0OLmtcF1ARd9tpnPyYwyNMZ6IOC5CQZwc';
-
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
@@ -30,11 +26,14 @@ export const messaging = firebase.messaging.isSupported()
   : null;
 
 if (messaging) {
-  messaging.onMessage(({ notification }) => {
-    const { title, body } = notification;
-    Toast.info({ title, description: body, duration: 0 });
-  });
+  messaging.usePublicVapidKey(
+    'BGwqAb_5soMWQmnpLrye4Guv74Wqj5jk9FqGBl2GZLl4FuU8EJgKEr0OLmtcF1ARd9tpnPyYwyNMZ6IOC5CQZwc'
+  );
 }
+messaging.onMessage(({ notification }) => {
+  const { title, body } = notification;
+  Toast.info({ title, description: body, duration: 0 });
+});
 
 messaging.onMessage(data => {
   console.log(data);
